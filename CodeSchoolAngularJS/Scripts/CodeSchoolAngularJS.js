98819980db3098f6b2e84e2873c56fec49cb165f
Gabriel Muñumel <gabrielmunumel@gmail.com>
2016-07-26 13:00:24 +0200

    Completed 2.7 Tabs Inside Out

--- CodeSchoolAngularJS/Scripts/CodeSchoolAngularJS.js
+++ CodeSchoolAngularJS/Scripts/CodeSchoolAngularJS.js
@@ -5,18 +5,51 @@
         this.products = gems;
     });
 
+    app.controller('PanelController', function () {
+        this.tab = 1;
+
+        this.selectTab = function (setTab) {
+            this.tab = setTab;
+        };
+        
+        this.isSelected = function (checkTab) {
+            return this.tab === checkTab;
+        };
+
+    });
+
     var gems = [
         {
             name: 'Dodecahedron',
             price: 2.95,
-            description: '...',
+            description: 'Lorem ipsum dolor sit amet, ' +
+                            'consectetur adipiscing elit, ' +
+                            'sed do eiusmod tempor incididunt ut ' +
+                            'labore et dolore magna aliqua. Ut enim' +
+                            'ad minim veniam, quis nostrud exercitation' +
+                            'ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            images: [
+                {
+                    full: '/Content/Images/dodecahedron.gif'
+                }
+            ],
             canPurchase: true,
-            soldOut: true,
+            soldOut: false,
         },
         {
             name: 'Pentagonal Gem',
             price: 5.95,
-            description: '...',
+            description: 'Lorem ipsum dolor sit amet, ' + 
+                            'consectetur adipiscing elit, ' + 
+                            'sed do eiusmod tempor incididunt ut ' +
+                            'labore et dolore magna aliqua. Ut enim' +
+                            'ad minim veniam, quis nostrud exercitation' +
+                            'ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            images: [
+                {
+                    full: '/Content/Images/pentagonal.gif'
+                }
+            ],
             canPurchase: false,
             soldOut: false,
         }
