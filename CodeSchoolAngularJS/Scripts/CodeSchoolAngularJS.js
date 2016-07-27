e98f09528bbf067e32cde8acfb2cb781e08510b2
Gabriel Muñumel <gabrielmunumel@gmail.com>
2016-07-27 15:21:06 +0200

    Added services and using

--- CodeSchoolAngularJS/Scripts/CodeSchoolAngularJS.js
+++ CodeSchoolAngularJS/Scripts/CodeSchoolAngularJS.js
@@ -1,9 +1,16 @@
 ﻿(function () {
     var app = angular.module('store', ['store-products']);
 
-    app.controller('StoreController', function () {
-        this.products = gems;
-    });
+    app.controller('StoreController', [ '$http', function ($http) {
+
+        var store = this;
+
+        store.products = [];
+
+        $http.get('/Models/Static/products.json').success(function (data) {
+            store.products = data;
+        });
+    }]);
 
     var gems = [
     {
